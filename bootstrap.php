495383e922535c74c64a8c902f6678a95ba58f6b
Lucas S Corte <corte1994.lc@gmail.com>
2021-04-28 08:08:57 -0300

    -criação do bootstrap.php

---
+++ bootstrap.php
@@ -0,0 +1,19 @@
+<?php
+
+require __DIR__."./vendor/autoload.php";
+
+$method = $_SERVER['REQUEST_METHOD'];
+$path = $_SERVER['PATH_INFO'] ??  '/';
+
+$route = new App\Router($method, $path);
+$route->get('/ola-{nome}',function($params){ return "Bem vindo(a) {$params[1]}"; });
+
+$result = $route->handler();
+
+if (!$result){
+    http_response_code(404);
+    echo "Página não encontrada";
+    die();
+}
+
+echo $result($route->getParams());
