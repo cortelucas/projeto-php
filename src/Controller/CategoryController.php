fb286e4a3d67d59c67b21913370850e27c497a71
Lucas S Corte <corte1994.lc@gmail.com>
2021-05-04 20:20:10 -0300

    - implementação controller de categorias - implementação da pagina inicial do categorias

---
+++ src/Controller/CategoryController.php
@@ -0,0 +1,12 @@
+<?php
+
+
+namespace App\Controller;
+
+
+class CategoryController
+{
+    public static function index(){
+        require '../src/View/Home/categorys.php';
+    }
+}
---
+++ src/View/Home/categorys.php
@@ -0,0 +1,5 @@
+<?php require '../src/View/cabecalho.php'?>
+
+    <h1 class="display-4 mt-5">Categorias</h1>
+
+<?php require '../src/View/rodape.php'?>
