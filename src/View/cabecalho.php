3c91d8492ea334934a540fd5f7e86fc809cc0c33
Lucas S Corte <corte1994.lc@gmail.com>
2021-05-04 21:05:59 -0300

    - implementação da classe Categoria.php

--- src/View/cabecalho.php
+++ src/View/cabecalho.php
@@ -23,7 +23,7 @@
         </button>
         <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
             <div class="navbar-nav">
-                <a class="nav-link active" aria-current="page" href="/">Home</a>
+                <a class="nav-link" aria-current="page" href="/">Home</a>
                 <a class="nav-link" href="/categorias">Categorias</a>
                 <a class="nav-link" href="/produtos">Produtos</a>
             </div>
